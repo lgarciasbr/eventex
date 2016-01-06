0e3a190bcbc9fb8d3ee995d39ff024469d6a401f
Leandro Garcia <leandro.garcias@gmail.com>
2016-01-06 21:18:48 -0200

    Ajuste no settings

--- eventex/settings.py
+++ eventex/settings.py
@@ -42,8 +42,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cleartest-without-migrations',
-    'django_extensions',
     'eventex.core',
     'eventex.subscriptions',
 )
