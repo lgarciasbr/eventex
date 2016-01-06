32b3f08779e0166fb2b922927ed2118350ddb647
Leandro Garcia <leandro.garcias@gmail.com>
2016-01-06 21:11:51 -0200

    Persiste a inscricao.

---
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+from django.test import TestCase
+from eventex.subscriptions.models import Subscription
+
+class SubscriptionModelTest(TestCase):
+    def setUp(self):
+        self.obj = Subscription(
+            name='Leandro Garcia',
+            cpf='12345678901',
+            email='leandro.garcias@gmail.com',
+            phone='19 991680300'
+        )
+        self.obj.save()
+
+    def test_create(self):
+        self.assertTrue(Subscription.objects.exists())
+
+    def test_created_at(self):
+        """Subscription must have an auto created_at attr."""
+        self.assertIsInstance(self.obj.created_at, datetime)
