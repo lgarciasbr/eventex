fb2b289efbb5b70bf8fadb7120f14fbde6956236
Leandro Garcia <leandro.garcias@gmail.com>
2016-01-24 12:09:31 -0200

    Grace HoOper > Grace HoPper

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     speakers = [
-        {'name': 'Grace Hooper', 'photo': 'http://hbn.link/hopper-pic'},
+        {'name': 'Grace Hopper', 'photo': 'http://hbn.link/hopper-pic'},
         {'name': 'Alan Turing', 'photo': 'http://hbn.link/turing-pic'}
     ]
     return render(request, 'index.html', {'speakers': speakers})
